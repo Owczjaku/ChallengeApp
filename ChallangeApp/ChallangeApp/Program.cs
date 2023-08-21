1c66ef4e7ae8fab1e4d6db3d7b5fa6cd615e87ab
Jakub Owczarczak <jakubowczarczak@gmail.com>
2023-08-21 22:13:01 +0200

    Zadanie domowe dzień 4

---
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var name = "Ewa";
+var gender = "Kobieta";
+var age = 27;
+
+if (gender == "Kobieta" && age <= 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
+
+else
+{
+    Console.WriteLine("Ktos zupełnie inny");
+
+}
