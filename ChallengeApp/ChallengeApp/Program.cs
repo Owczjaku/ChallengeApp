5be7f9de1d2af77961522c08dcbf6bcbf7fe7afe
Jakub Owczarczak <jakubowczarczak@gmail.com>
2023-08-28 16:42:12 +0200

    Praca domowa dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,73 +1,48 @@
-﻿
-int number = 836121681;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallengeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee employee1 = new Employee("Jakub", "Rak", 41 );
+Employee employee2 = new Employee("Joanna", "Nowak", 27);
+Employee employee3 = new Employee("Paweł", "Przybył", 33);
 
-foreach (char letter in letters)
 
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
+employee1.AddScore(4);
+employee1.AddScore(7);
+employee1.AddScore(5);
+employee1.AddScore(1);
+employee1.AddScore(9);
 
-    else if (letter == '2')
-    {
-        counter2++;
-    }
 
-    else if (letter == '3')
-    {
-        counter3++;
-    }
+employee2.AddScore(2);
+employee2.AddScore(7);
+employee2.AddScore(4);
+employee2.AddScore(5);
+employee2.AddScore(3);
 
-    else if (letter == '4')
-    {
-        counter4++;
-    }
 
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+employee3.AddScore(3);
+employee3.AddScore(9);
+employee3.AddScore(6);
+employee3.AddScore(5);
+employee3.AddScore(3);
 
-Console.WriteLine("0 =>" + counter0++);
-Console.WriteLine("1 =>" + counter1++);
-Console.WriteLine("2 =>" + counter2++);
-Console.WriteLine("3 =>" + counter3++);
-Console.WriteLine("4 =>" + counter4++);
-Console.WriteLine("5 =>" + counter5++);
-Console.WriteLine("6 =>" + counter6++);
-Console.WriteLine("7 =>" + counter7++);
-Console.WriteLine("8 =>" + counter8++);
-Console.WriteLine("9 =>" + counter9++);
+List<Employee> employees = new List<Employee>()
+{
+
+    employee1, employee2, employee3
+
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+
+    if(employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+
+    }    
+}
+
+Console.WriteLine("Pracownikem z najwyższym wynikiem jest: " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + ", wiek: " + employeeWithMaxResult.Age + ", punkty: " + employeeWithMaxResult.Result);
