242674099e13c2a93f59ba6c7b09f50ab8542d54
Jakub Owczarczak <jakubowczarczak@gmail.com>
2023-09-01 17:22:06 +0200

    Zadanie domowe dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,34 +1,97 @@
-﻿namespace ChallengeApp
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChallengeApp
 {
     public class Employee
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
+        private List<float> grades = new List<float>(); 
 
-        private List<int> score = new List<int>();
-
-        public Employee(string name, string surname, int age)
+        public Employee(string name, string surname)
+        
         {
-
             this.Name = name;
-            this.Surname = surname;
-            this.Age = age;
+            this.Surname = surname; 
+        
         }
+        
 
-        public void AddScore(int number)
-        {
-            this.score.Add(number);
-        }
+        public string Name { get; private set; }    
+        public string Surname { get; private set; }
 
-        public int Result
+        public void AddGrade(float grade)
         {
-            get
+
+
+            if (grade >= 0 && grade <= 100)
+
             {
-                return this.score.Sum();
-            } 
+
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+        
+        public void AddGrade(string grade)
+        {
+            
+            
+                if(float.TryParse(grade, out float result))
+                    {
+                    this.AddGrade(result);
+                    }
+                else
+            {
+                Console.WriteLine("String if not float");
+            }    
+                
+            
         }
 
+        public void AddGrade(double grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
+        public void AddGrade(long grade)
+        {
+            if (grade >= 0 && grade <= 500)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade value");
+            }
+
+        }
+
+
+
+
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach (var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+
+            statistics.Average /=  this.grades.Count;
+
+            return statistics;
+        }
 
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,59 +1,22 @@
 ﻿
 using ChallengeApp;
-{ 
-var employee = new Employee("Jakub", "Rak");
-employee.AddGrade(4);
-employee.AddGrade(5);
-employee.AddGrade(1);
-employee.AddGrade(9);
-
-
-var statistics = employee.GetStatistics();
-
-Console.WriteLine($"Min:  + {statistics.Min}");
-
-
-
-void SetSth(Statistics statistics)
 {
-    statistics = new Statistics();  
+    var employee = new Employee("Jakub", "Rak");
+    employee.AddGrade("Jakub");
+    employee.AddGrade("4000");
+    employee.AddGrade(2);
+    employee.AddGrade(5.5);
+    employee.AddGrade(444);
+
+
+    var statistics = employee.GetStatistics();
+
+    Console.WriteLine($"Min:  + {statistics.Min}");
+    Console.WriteLine($"Max:  + {statistics.Max}");
+    Console.WriteLine($"Average:  + {statistics.Average}", "N2");
 }
 
-}
-
-{ 
-var employee = new Employee("Alicja", "Polak");
-employee.AddGrade(2);
-employee.AddGrade(7);
-employee.AddGrade(8);
-employee.AddGrade(4);
 
 
-var statistics = employee.GetStatistics();
 
-Console.WriteLine($"Max:  + {statistics.Max}");
 
-void SetSth2(Statistics statistics)
-    { 
-    statistics = new Statistics();
-    }
-}
-
-{ 
-
-var employee = new Employee("Jan", "Nowak");
-employee.AddGrade(6);
-employee.AddGrade(6);
-employee.AddGrade(3);
-employee.AddGrade(3);
-
-var statistics = employee.GetStatistics();
-
-Console.WriteLine($"Average:  + {statistics.Average}");
-
-void SetSth3(Statistics statistics)
-{
-    statistics = new Statistics();
-}
-
-}
